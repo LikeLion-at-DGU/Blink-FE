3b24214181d6d0a7e1c23b1fa2698495aee5cacf
오찬주 <ohchanju3@naver.com>
2023-08-16 03:55:04 +0900

    design: 인트로 페이지 애니메이션

--- blink_FE/src/pages/Intro/intro.jsx
+++ blink_FE/src/pages/Intro/intro.jsx
@@ -2,81 +2,15 @@ import React, { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
 import Logo from "../../assets/images/NuLogo.png";
 import Locationpin from "../../assets/images/location.png";
+import Magnifying from "../../assets/images/magnify.png";
+import Camera from "../../assets/images/camera.png";
 import * as S from "./style";
-// Define keyframes animation
-export const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`;
-
-export const fadeOut = keyframes`
-  from {
-    opacity: 1;
-  }
-  to {
-    opacity: 0;
-  }
-`;
-
-export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 2rem;
-  background-color: ${({ bgColor }) => bgColor};
-  opacity: ${({ opacity }) => opacity};
-  color: ${({ textColor }) => textColor};
-  animation: ${({ isFadingIn }) => (isFadingIn ? fadeIn : fadeOut)} 2s
-    ease-in-out;
-`;
-
-export const LogoImage = styled.img`
-  width: 20rem;
-  height: 20rem;
-`;
-
-export const LogoText = styled.div`
-  margin-top: 5rem;
-  line-height: 2.5rem;
-  text-align: center;
-  font-family: "GangwonEduPowerExtraBoldA";
-`;
-
-export const LogoBlink = styled.div`
-  margin-top: 1rem;
-  line-height: 2.5rem;
-  text-align: center;
-  font-size: 3rem;
-  font-weight: bolder;
-`;
-
-export const LocationImg = styled.img`
-  width: 20rem;
-  height: 20rem;
-  z-index: 100;
-`;
-
-export const LocationTextTitle = styled.div`
-  color: white;
-  font-weight: bolder;
-`;
-
-const onClick = () => {
-  history.push("/main");
-};
 
 const Page1 = () => (
   <S.Container bgColor="#1B2130" textColor="white" isFadingIn={true}>
     <S.LogoImage src={Logo} alt="Logo" />
     <S.LogoText>
-      내 블랙박스가 세상의 눈이 된다면
+      블랙박스로 세상을 연결해봐요!
       <br />
       지도 기반 블랙박스 영상 공유 플랫폼
     </S.LogoText>
@@ -84,24 +18,173 @@ const Page1 = () => (
   </S.Container>
 );
 
-const Page2 = () => (
-  <S.TwoContainer bgColor="#34446D" textColor="#333" isFadingIn={true}>
-    <S.LocationImg src={Locationpin} alt="Location" />
-    <S.LocationTextTitle>
-      사라진.. 전동킥보드를 찾습니다 ...!
-    </S.LocationTextTitle>
-  </S.TwoContainer>
-);
+const Page2 = () => {
+  const [isVisible, setIsVisible] = useState(false);
 
-const Page3 = () => (
-  <S.Container bgColor="#1B2130" textColor="white" isFadingIn={true}>
-    <S.LocationImg src={Locationpin} alt="Location" />
-    <S.LocationTextTitle>
-      사라진.. 전동킥보드를 찾습니다 ...!
-    </S.LocationTextTitle>
-    <S.Button onClick={onClick}>Go to Main Page</S.Button>
-  </S.Container>
-);
+  const handleScroll = () => {
+    const viewportHeight = window.innerHeight;
+    const pageTop = viewportHeight * 0.5; // 50% 위치 계산
+
+    if (window.scrollY >= pageTop) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleAnimationEnd = () => {
+    setIsVisible(false);
+  };
+
+  return (
+    <S.ThreeContainer bgColor="#34446D" textColor="white" isFadingIn={true}>
+      <S.LocationImg
+        src={Camera}
+        alt="Location"
+        onAnimationEnd={handleAnimationEnd}
+        style={{
+          opacity: isVisible ? 1 : 0,
+          transform: isVisible ? "translateY(0)" : "translateY(100px)",
+          transition: "opacity 1s ease, transform 1s ease",
+        }}
+      />
+      <S.WholeLocationText>
+        <S.LocationTextTitle>사고는 갑자기 찾아온답니다</S.LocationTextTitle>
+        <S.LocationText>
+          내 사고장면을 찍어준 다른 사람이 있다면 얼마나 고마울까요?
+          <br />
+          번거로운 보험, 법적 절차를 간단하게 만들어줄 수 있는 영상이
+          <br />
+          빠르게 제보된다면 얼마나 편할까요?
+        </S.LocationText>
+      </S.WholeLocationText>
+      {/* <S.Button onClick={onClick}>Go to Main Page</S.Button> */}
+    </S.ThreeContainer>
+  );
+};
+
+const Page3 = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const viewportHeight = window.innerHeight;
+    const pageTop = viewportHeight * 1.0; // 50% 위치 계산
+
+    if (window.scrollY >= pageTop) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleAnimationEnd = () => {
+    setIsVisible(false);
+  };
+
+  return (
+    <S.ThreeContainer bgColor="#1B2130" textColor="white" isFadingIn={true}>
+      <S.WholeLocationText>
+        <S.LocationTextTitle>
+          사라진.. 전동킥보드를 찾습니다 ...!
+        </S.LocationTextTitle>
+        <S.LocationText>
+          사라진 내 킥보드를 찾고 싶으신가요? <br />
+          혹은,사고로 인해 증거영상이 필요하신가요? <br />
+          특정 지점에 대한 블랙박스를 요청해보세요!
+        </S.LocationText>
+      </S.WholeLocationText>
+      <S.LocationImg
+        src={Magnifying}
+        alt="Location"
+        onAnimationEnd={handleAnimationEnd}
+        style={{
+          opacity: isVisible ? 1 : 0,
+          transform: isVisible ? "translateY(0)" : "translateY(100px)",
+          transition: "opacity 1s ease, transform 1s ease",
+        }}
+      />
+      {/* <S.Button onClick={onClick}>Go to Main Page</S.Button> */}
+    </S.ThreeContainer>
+  );
+};
+
+const Page4 = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const viewportHeight = window.innerHeight;
+    const pageTop = viewportHeight * 1.8; // 50% 위치 계산
+
+    if (window.scrollY >= pageTop) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleAnimationEnd = () => {
+    setIsVisible(false);
+  };
+
+  const handleGoToMainPage = () => {
+    // 메인 페이지로 이동하는 코드를 여기에 작성
+    // 예: window.location.href = "/"; 를 사용해서 메인 페이지로 이동할 수 있습니다.
+  };
+
+  return (
+    <S.PageWrapper bgColor="#34446D" textColor="#333" isFadingIn={true}>
+      <S.TwoContainer>
+        <S.LocationImg
+          src={Locationpin}
+          alt="Location"
+          onAnimationEnd={handleAnimationEnd}
+          style={{
+            opacity: isVisible ? 1 : 0,
+            transform: isVisible ? "translateY(0)" : "translateY(100px)",
+            transition: "opacity 1s ease, transform 1s ease",
+          }}
+        />
+        <S.WholeLocationText>
+          <S.LocationTextTitle>
+            블랙박스로 서로의 눈과 귀가 되어주세요!
+          </S.LocationTextTitle>
+          <S.LocationText>
+            <br /> 당신의 제보가 필요한 사람들이 많습니다. <br />
+            누군가에게 중요한 사람이 되어주세요. <br />
+            그리고 다시 보답받으세요.
+          </S.LocationText>
+        </S.WholeLocationText>
+      </S.TwoContainer>
+      <S.GoMain>
+        <S.Button onClick={handleGoToMainPage}>Blink 시작하기</S.Button>
+      </S.GoMain>
+    </S.PageWrapper>
+  );
+};
 
 function App() {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -123,6 +206,7 @@ function App() {
       <Page1 />
       <Page2 />
       <Page3 />
+      <Page4 />
     </>
   );
 }
