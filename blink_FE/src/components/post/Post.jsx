36c0418f970297a30e7eb056ed1ac2c7cfda9ce9
Jyo <2019112966@dongguk.edu>
2023-08-17 04:05:38 +0900

    달력반쯤복구..

--- blink_FE/src/components/post/Post.jsx
+++ blink_FE/src/components/post/Post.jsx
@@ -1,26 +1,23 @@
-//Post.jsx
-// #1 메인 - 글쓰기 페이지 총괄
-
 import React, { useState } from "react";
 import styled from "styled-components";
 import AdrSearch from "./AdrSearch";
 import { StyledSearchResult, SearchResultInputs } from "./SearchResult";
-import Calendartwo from "./DatePicker";
 import FileUpload from "./FileUpload";
 import { BiSearchAlt2 } from "react-icons/bi";
 import { AiOutlineCloudUpload } from "react-icons/ai";
 import HorizonLine from "./Line";
+import Calendartwo from "./DatePicker";
 
 class Question extends React.Component {
   render() {
     return (
       <h3>
-        {" "}
-        Lets go for a <BiSearchAlt2 />?{" "}
+        Lets go for a <BiSearchAlt2 />?
       </h3>
     );
   }
 }
+
 
 const Outer = styled.div`
   height: 1080px;
@@ -156,17 +153,6 @@ const TitleInput = styled.input`
   border: none;
 `;
 
-const Search2 = styled(Search)`
-  width: 295px;
-  height: 56px;
-  margin-left: 30px;
-  display: flex;
-  justify-content: space-between;
-  padding: 0 20px 0 20px;
-  font-size: 27px;
-  cursor: pointer;
-`;
-
 const TextArea = styled.textarea`
   width: 100%;
   height: 220px;
@@ -175,7 +161,6 @@ const TextArea = styled.textarea`
   resize: none;
   font-size: 27px;
 `;
-
 
 const Thumbnail = styled.img`
   max-width: 250px;
@@ -190,7 +175,7 @@ const ThumbnailsContainer = styled.div`
 
 const CustomCloudUploadIcon = styled(AiOutlineCloudUpload)`
   font-size: 40px;
-  margin-left:40px;
+  margin-left: 40px;
 `;
 
 const UploadText = styled.div`
@@ -209,31 +194,37 @@ const RegisterButton = styled.button`
   box-shadow: 3px 3px 3px gray;
 `;
 
-export default function Post() {
+const Search2 = styled(Search)`
+  width: 295px;
+  height: 56px;
+  margin-left: 30px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px 0 20px;
+  font-size: 27px;
+  cursor: pointer;
+`;
 
+export default function Post() {
   const handleAddressSearchClick = () => {
     console.log("handleAddressSearchClick is triggered");
     setShowAdrSearch(true);
   };
-
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [showDatePicker, setShowDatePicker] = useState(false);
   const [showAdrSearch, setShowAdrSearch] = useState(false);
   const [addressInfo, setAddressInfo] = useState({
     postcode: "",
     address: "",
   });
 
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [selectedAddress, setSelectedAddress] = useState("");
+  const handleDateSelection = (date) => {
+    setSelectedDate(date);
+    setShowDatePicker(false); // Close the calendar
+  };
 
   const toggleAdrSearch = () => {
     setShowAdrSearch(!showAdrSearch);
-    setShowDatePicker(false); // Hide the date picker when toggling AdrSearch
-  };
-
-  const handleDatePickerSelect = (date) => {
-    setSelectedDate(date); // Update the selected date
-    setShowDatePicker(false); // Hide the date picker
   };
 
   const handleComplete = (data) => {
@@ -246,9 +237,7 @@ export default function Post() {
     };
 
     setAddressInfo(updatedAddressInfo);
-    onUpdateAddress(updatedAddressInfo);
-    setSelectedAddress(updatedAddressInfo.address); // Update selected address
-    setShowAdrSearch(false); // Close the AdrSearch window after address selection
+    setShowAdrSearch(false);
   };
 
   const [uploadedFiles, setUploadedFiles] = useState([]);
@@ -261,6 +250,7 @@ export default function Post() {
 
     setUploadedFiles(files);
   };
+
   const handleRemoveThumbnail = (index) => {
     const updatedFiles = [...uploadedFiles];
     updatedFiles.splice(index, 1);
@@ -269,7 +259,7 @@ export default function Post() {
 
   const [isReportChecked, setIsReportChecked] = useState(true);
   const [isLookForChecked, setIsLookForChecked] = useState(false);
-  
+
   const handleReportCheckboxChange = () => {
     setIsReportChecked(!isReportChecked);
     setIsLookForChecked(false);
@@ -279,16 +269,20 @@ export default function Post() {
     setIsLookForChecked(!isLookForChecked);
     setIsReportChecked(false);
   };
+  
+  const handleDatePickerClick = () => {
+    setShowDatePicker(!showDatePicker);
+  };
 
   return (
     <Outer>
       <CheckDisplay>
         <Check onClick={handleReportCheckboxChange}>
-          제보해요 &nbsp;
+          제보해요
           <Checkbox type="checkbox" checked={isReportChecked} />
         </Check>
         <Check onClick={handleLookForCheckboxChange}>
-          찾아요 &nbsp;&nbsp;
+          찾아요
           <Checkbox type="checkbox" checked={isLookForChecked} />
         </Check>
       </CheckDisplay>
@@ -296,36 +290,26 @@ export default function Post() {
         <TopRow>
           <Search
             onClick={toggleAdrSearch}
-            selectedAddress={addressInfo.address} // Pass the selected address
+            selectedAddress={addressInfo.address}
           >
             {addressInfo.address
               ? addressInfo.address
               : "지역명, 도로명, 주소를 입력해주세요."}
             <BiSearchAlt2 />
           </Search>
+          <Search2 onClick={handleDatePickerClick}>촬영 or 요청일자<BiSearchAlt2 /></Search2>
+      </TopRow>
 
-          <Search2 onClick={() => setShowDatePicker(!showDatePicker)}>
-            {selectedDate
-              ? selectedDate.toLocaleDateString()
-              : "촬영 or 요청일자"}
-            <BiSearchAlt2 />
-          </Search2>
-        </TopRow>
-
-        <AdrSearchContainer show={showAdrSearch || showDatePicker}>
-          {showAdrSearch ? (
+        <AdrSearchContainer show={showAdrSearch}>
+          {showAdrSearch && (
             <AdrSearch
               onUpdateAddress={setAddressInfo}
               showAdrSearch={showAdrSearch}
-              setShowAdrSearch={setShowAdrSearch} // Pass the function here
+              setShowAdrSearch={setShowAdrSearch}
             />
-          ) : (
-            showDatePicker && (
-              <Calendartwo onSelectDate={handleDatePickerSelect} />
-            )
           )}
         </AdrSearchContainer>
-
+        {showDatePicker && <Calendartwo user="your_user_here" />}
         {showAdrSearch && (
           <StyledSearchResult>
             <SearchResultInputs
@@ -349,7 +333,7 @@ export default function Post() {
                 <TitleInput type="text" placeholder="제목을 입력해주세요." />
               </FormRow>
               <FormRow>
-                <Select>
+              <Select>
                   <option value="" disabled selected hidden>
                     카테고리
                   </option>
@@ -372,7 +356,7 @@ export default function Post() {
             <>
               <CustomCloudUploadIcon />
               <UploadText>
-                .mov, .mp4 .png, .jpg, .jpeg, .pdf 파일을 업로드해주세요.
+              .mov, .mp4 .png, .jpg, .jpeg, .pdf 파일을 업로드해주세요.
                 <br />
                 <br />
                 최대 2장까지 업로드할 수 있습니다.
@@ -390,7 +374,7 @@ export default function Post() {
           <br />
         </SquareBox2>
       </PostContainer>
-      <RegisterButton>등록하기</RegisterButton>
+      <RegisterButton>Register</RegisterButton>
     </Outer>
   );
 }
