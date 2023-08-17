cbb1ef52725fed83f55f55a9a1c553672da76025
Jyo <2019112966@dongguk.edu>
2023-08-17 19:46:37 +0900

    post프론트끝

--- blink_FE/src/components/post/DatePicker.jsx
+++ blink_FE/src/components/post/DatePicker.jsx
@@ -13,27 +13,52 @@ const Container = styled.div`
   background-color: black;
 `;
 
-export default function Calendartwo({ user }) {
+const DotBox = styled.div`
+  width: 100%;
+  height: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CalendarDot = styled.div`
+  margin-top: 5px;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: #f87171;
+`;
+
+export default function Calendartwo({ user, onSelectDate }) {
   const [value, onChange] = useState(new Date());
-  const dateArr = ["2023. 08. 15.", "2023. 08. 17.", "2023. 09. 02."];
+  const dateArr = ["2023. 08. 15.", "2023. 08. 17."];
+
+  const handleDateSelect = (date) => {
+    onChange(date);
+    const formattedDate = date.toLocaleDateString("en", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+    onSelectDate(formattedDate); // Call the onSelectDate prop with the selected date
+  };
+
   return (
     <Container>
       <Calendar
-        onChange={onChange}
+        onChange={handleDateSelect} // Use handleDateSelect as the onChange handler
         value={value}
         formatDay={(locale, date) =>
-          //xx일 -> xx 으로 format 변경
           new Date(date).toLocaleDateString("en-us", {
             day: "2-digit",
           })
         }
         tileContent={({ date, view }) => {
-          //
           const exist = dateArr.find(
             (oneDate) =>
               oneDate ===
               String(
-                new Date(date).toLocaleDateString("ko", {
+                new Date(date).toLocaleDateString("en", {
                   year: "numeric",
                   month: "2-digit",
                   day: "2-digit",
@@ -42,6 +67,7 @@ export default function Calendartwo({ user }) {
           );
           return (
             <>
+              <DotBox>{exist && <CalendarDot />}</DotBox>
             </>
           );
         }}
