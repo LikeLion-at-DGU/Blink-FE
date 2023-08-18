84e36d2ca14e5b1daa9cd55cf03fc0bd6df2daef
leechangjoon <2018110021@dongguk.edu>
2023-08-18 17:23:01 +0900

    Fix: Media image 오류 해결중

--- blink_FE/src/components/Post2/Post2.jsx
+++ blink_FE/src/components/Post2/Post2.jsx
@@ -264,6 +264,7 @@ const Post2 = (props) => {
       .get(`/api/mainposts/${mainpostId}`)
       .then((response) => {
         setPostDetail(response.data); // 가져온 데이터를 상태에 저장
+        console.log(response.data);
       })
       .catch((error) => {
         console.error("Error fetching post data:", error);
@@ -301,7 +302,17 @@ const Post2 = (props) => {
         </Context>
       </PostBox>
 
-      <ClipBox>첨부파일 자리</ClipBox>
+      <ClipBox>
+        {postDetail.medias &&
+          postDetail.medias.map((mediaObj, index) => (
+            <img
+              key={index}
+              src={mediaObj.media}
+              alt={`Media ${index}`}
+              style={{ width: "100%", height: "auto" }}
+            />
+          ))}
+      </ClipBox>
       <CommentBox>
         <CommentTitle>댓글</CommentTitle>
         <CommentForm onSubmit={handleSubmitComment}>
